f6b1523c8b7de95df97e066b46fc56c7b65a5387
Sanya <sasha141412@gmail.com>
2023-02-08 11:14:31 +0300

    v1

---
+++ src/main/java/ru/vasiliev/springcourse/FirstSecurityApp/Controllers/AdminController.java
@@ -0,0 +1,49 @@
+package ru.vasiliev.springcourse.FirstSecurityApp.Controllers;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import ru.vasiliev.springcourse.FirstSecurityApp.models.User;
+import ru.vasiliev.springcourse.FirstSecurityApp.repositories.RoleRepository;
+import ru.vasiliev.springcourse.FirstSecurityApp.services.UserService;
+
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final RoleRepository roleRepository;
+    private final UserService userService;
+
+    public AdminController(RoleRepository roleRepository, UserService userService) {
+        this.roleRepository = roleRepository;
+        this.userService = userService;
+    }
+
+    @GetMapping()
+    public String startPageAdmin(@AuthenticationPrincipal User user, ModelMap model) {
+        model.addAttribute("user", user);
+        model.addAttribute("users", userService.findAll());
+        model.addAttribute("roles", roleRepository.findAll());
+        return "pages/admin";
+    }
+
+    @PostMapping("/new")
+    public String performRegistration(@ModelAttribute("user") User user) {
+        userService.saveUser(user);
+        return "redirect:/admin/";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("user") User user, @PathVariable("id") int id) {
+        userService.update(id, user);
+        return "redirect:/admin/";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") int id) {
+        userService.delete(id);
+        return "redirect:/admin/";
+    }
+}
---
+++ src/main/java/ru/vasiliev/springcourse/FirstSecurityApp/services/UserService.java
@@ -0,0 +1,49 @@
+package ru.vasiliev.springcourse.FirstSecurityApp.services;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.vasiliev.springcourse.FirstSecurityApp.models.User;
+import ru.vasiliev.springcourse.FirstSecurityApp.repositories.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public User findOne(int id) {
+        Optional<User> foundUser = userRepository.findById(id);
+        return foundUser.orElse(new User());
+    }
+
+    public void update(int id, User updateUser) {
+        updateUser.setId(id);
+        if (updateUser.getPassword() == null) {
+            updateUser.setPassword(findOne(id).getPassword());
+        } else {
+            updateUser.setPassword(passwordEncoder.encode(updateUser.getPassword()));
+        }
+        userRepository.save(updateUser);
+    }
+
+    public void delete(int id) {
+        userRepository.deleteById(id);
+    }
+}
